bc9809f6ed05b35e394e67c3569eefb3d6262028
MickDuprez <mickduprez@gmail.com>
2022-10-10 13:41:01 +1100

    first commit

---
+++ api/src/services/comments/comments.test.js
@@ -0,0 +1,23 @@
+import { comments } from './comments'
+import { db } from 'src/lib/db'
+
+// Generated boilerplate tests do not account for all circumstances
+// and can fail without adjustments, e.g. Float and DateTime types.
+//           Please refer to the RedwoodJS Testing Docs:
+//       https://redwoodjs.com/docs/testing#testing-services
+// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
+
+describe('comments', () => {
+  scenario(
+    'returns all comments for a single post from the db',
+    async (scenario) => {
+      const result = await comments({ postId: scenario.comment.jane.postId })
+
+      const post = await db.post.findUnique({
+        where: { id: scenario.comment.jane.postId },
+        include: { comments: true },
+      })
+      expect(result.length).toEqual(post.comments.length)
+    }
+  )
+})
